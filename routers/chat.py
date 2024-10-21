5bdb36a3f65315e7da35d8c472b39ed996ac6d5f
phuchb204 <h.baophuc297@gmail.com>
2024-10-21 18:11:35 +0700

    Update chat.py

--- routers/chat.py
+++ routers/chat.py
@@ -2,9 +2,6 @@ from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
 from sqlalchemy.orm import Session
 from typing import List
 
-from ..schemas.chat import ChatRoomCreate, MessageCreate, ChatRoom, Message
-from ..models import chat, user
-from services. import ChatService
-from ..dependencies import get_db, get_current_user
+
 
 router = APIRouter()
