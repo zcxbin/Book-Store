aa89f01982865c666f680a256c71c4f394ddeba2
zcxbin <zcxbin@gmail.com>
2024-10-24 04:15:23 +0700

    change search bÃook by name -> by id

--- routers/book.py
+++ routers/book.py
@@ -20,14 +20,14 @@ async def get_books(db=Depends(get_db),
         raise HTTPException(status_code=500, detail="Book not found")
 
 
-@router.get('/get_books_by_author_name', response_model=BookResponse)
-async def get_books_by_author_name(
-        author_name: str,
+@router.get('/get_books_by_author_id', response_model=BookResponse)
+async def get_books_by_author_id(
+        author_id: str,
         db=Depends(get_db),
         book_service=Depends(get_book_service)):
     try:
 
-        books = book_service.get_books_by_author_name(db, author_name)
+        books = book_service.get_books_by_author_id(db, author_id)
         if not books:
             raise HTTPException(status_code=404, detail="Author not found")
         return BookResponse(
@@ -39,17 +39,17 @@ async def get_books_by_author_name(
         raise HTTPException(status_code=500, detail="An error occurred while fetching books by author name.")
 
 
-@router.get('/get_books_by_category_name', response_model=BookResponse)
-async def get_books_by_category_name(
-        category_name: str,
+@router.get('/get_books_by_category_id', response_model=BookResponse)
+async def get_books_by_category_id(
+        category_id: str,
         db=Depends(get_db),
         book_service=Depends(get_book_service)):
     try:
-        books = book_service.get_books_by_category_name(db, category_name)
+        books = book_service.get_books_by_category_id(db, category_id)
         return BookResponse(
             books=books,
             length=len(books)
         )
     except Exception as e:
         print(e)
-        raise HTTPException(status_code=500, detail="An error occurred while fetching books by category name.")
+        raise HTTPException(status_code=500, detail="An error occurred while fetching books by category_id name.")
--- services/book_service.py
+++ services/book_service.py
@@ -3,7 +3,6 @@ from exceptions import raise_error
 from models import Category as CategoryModel
 from models.book import Book as BookModel
 from models.author import Author as AuthorModel
-from schemas.book import BookResponse
 
 
 def get_book_service():
@@ -18,18 +17,18 @@ class BookService:
     def get_all_books(self, db: Session):
         return db.query(BookModel).all()
 
-    def get_books_by_author_name(self, db: Session, author_name: str):
+    def get_books_by_author_id(self, db: Session, author_id: int):
         author_model = db.query(AuthorModel).filter(
-            AuthorModel.author_name == author_name).first()
+            AuthorModel.id == author_id).first()
 
         if not author_model:
             raise_error(101)
 
         return db.query(BookModel).filter(BookModel.author_id == author_model.id).all()
 
-    def get_books_by_category_name(self, db: Session, category_name: str):
+    def get_books_by_category_id(self, db: Session, category_id: int):
         category_model = db.query(CategoryModel).filter(
-            CategoryModel.type_name == category_name).first()
+            CategoryModel.id == category_id).first()
 
         if not category_model:
             raise_error(201)
