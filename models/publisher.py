9696d650fda5a1cc40c39da21e9222236ce29fe5
zcxbin <zcxbin@gmail.com>
2024-11-09 23:18:32 +0700

    update

--- models/publisher.py
+++ models/publisher.py
@@ -7,7 +7,7 @@ from configs.database import Base
 class Publisher(Base):
     __tablename__ = 'publishers'
     id = Column(Integer, primary_key=True, index=True, nullable=False)
-    name = Column(String(100), nullable=False)
+    publisher_name = Column(String(100), nullable=False)
     country = Column(String(100), nullable=False)
 
     books = relationship('Book', back_populates='publishers')
